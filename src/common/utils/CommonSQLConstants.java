4118ed72ad21d36e74f023f719e5c6211859df9d
Sunil <vishkarmasunil@gmail.com>
2016-10-28 11:13:25 +0530

    Revert "OnlineMedicalSystem"
    This reverts commit a52d22772eb3dd1c275eabb5e349134f3796dcf6.

--- src/common/utils/CommonSQLConstants.java
+++
@@ -1,6 +0,0 @@
-package common.utils;
-public interface CommonSQLConstants 
-{
-public static String LOGIN_SQLADMIN="select adminemail,adminpassword, adminfirstname, adminlastname from admin_db where adminemail=? and adminpassword=?";
-public static String LOGIN_SQLUSER="select c_email,c_password, c_firstname, c_lastname from user_db where c_email=? and c_password=?";
-}
